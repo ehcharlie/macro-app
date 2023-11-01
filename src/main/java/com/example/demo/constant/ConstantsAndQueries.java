0735e02d5189e2aeaff6a7bda15af5036775c4b7
cxhook <cxhook@mclane.mclaneco.com>
2023-11-01 16:54:35 -0400

    updating api/api key

--- src/main/java/com/example/demo/constant/ConstantsAndQueries.java
+++ src/main/java/com/example/demo/constant/ConstantsAndQueries.java
@@ -2,8 +2,8 @@ package com.example.demo.constant;
 
 public class ConstantsAndQueries {
 
-    public static final String NUTRITION_URL = "https://api.calorieninjas.com/v1/nutrition?query=";
+    public static final String NUTRITION_URL = "https://api.api-ninjas.com/v1/nutrition?query=";
 
-    public static final String RECIPE_URL = "https://api.calorieninjas.com/v1/recipe?query=";
+    public static final String RECIPE_URL = "https://api.api-ninjas.com/v1/recipe?query=";
 
 }
