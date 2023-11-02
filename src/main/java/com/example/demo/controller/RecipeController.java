1e909235096489ed528faf5325de3e8dbedc871b
rparmater7 <parmater11@gmail.com>
2023-11-02 15:41:38 -0500

    Adding recipe search, first commit!

--- src/main/java/com/example/demo/controller/RecipeController.java
+++ src/main/java/com/example/demo/controller/RecipeController.java
@@ -1,36 +1,24 @@
 package com.example.demo.controller;
 
-import com.example.demo.constant.ConstantsAndQueries;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import com.example.demo.model.Recipe;
+import com.example.demo.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/recipes")
 public class RecipeController {
 
-    @Value("${api-key}")
-    private String userApiKey;
+    @Autowired
+    RecipeService recipeService;
 
-    //TODO: Convert this into a serializable object to return response
-    @GetMapping()
-    public ResponseEntity getRecipe() {
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.add("x-api-key", userApiKey);
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        return restTemplate.exchange(
-                ConstantsAndQueries.RECIPE_URL + "pork",
-                HttpMethod.GET,
-                new HttpEntity<>(headers),
-                Object.class);
+    @GetMapping("/{recipe}")
+    public List<Recipe> getRecipe(@PathVariable String recipe) {
+        return recipeService.getRecipe(recipe);
     }
 }
